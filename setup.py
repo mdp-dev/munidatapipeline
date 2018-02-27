29988ce84ce783f12d676ab1f8f8e309e881f58c
clandau <clandau@local>
2018-02-27 15:50:51 +0000

    setup.py update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='munidatapipeline',
-      version='00.01.0745',
+      version='00.01.11',
       description='An ETL tool for working with municipal open data (especially GiS data) on resource constrained systems.',
       author='Charles Landau, Andrew Gobbi',
       author_email='munidatapipelinedevelopment@gmail.com',
